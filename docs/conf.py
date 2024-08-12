7ae9fe4d9c2dba4d57b9a62215a730735cfbf7bc
Philipp A. <flying-sheep@web.de>
2024-08-12 13:58:27 +0200

    Run nb in env

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,10 @@
 
 from __future__ import annotations
 
+from functools import partial
 from importlib.metadata import metadata
 from importlib.util import find_spec
+from subprocess import run
 
 _sbc = (
     ["sphinx_build_compatibility.extension"]
@@ -25,6 +27,24 @@ nitpicky = True
 suppress_warnings = ["mystnb.unknown_mime_type"]
 
 # myst_nb settings
-nb_execution_mode = "cache"
+nb_execution_mode = "force"
 nb_execution_show_tb = True
 # For debugging: nb_execution_timeout = -1
+
+
+# https://github.com/executablebooks/MyST-NB/issues/574
+def _patch_myst_nb() -> None:
+    from jupyter_cache.executors import utils  # type: ignore[import-not-found]
+    from myst_nb.core.execute import cache, direct  # type: ignore[import-not-found]
+
+    run(  # noqa: S603
+        ["hatch", "-v", "run", "notebook:install-kernel"],  # noqa: S607
+        check=True,
+    )
+
+    cache.single_nb_execution = direct.single_nb_execution = partial(
+        utils.single_nb_execution, kernel_name="session-info2"
+    )
+
+
+_patch_myst_nb()
